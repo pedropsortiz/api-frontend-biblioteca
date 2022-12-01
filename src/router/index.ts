edf9ecb156408d356cd2be74179e3061d35ebb59
Pedro Ortiz <pedrops.ortiz@gmail.com>
2022-11-30 21:48:41 -0300

    Criação do form de cadastro de livro

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import AdicionarLivroView from "../views/Livro/AdicionarLivroView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: "",
     },
     {
       path: "/about",
@@ -15,7 +15,12 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: "",
+    },
+    {
+      path: "/admin/livro/adicionar",
+      name: "AdicionarLivro",
+      component: AdicionarLivroView
     },
   ],
 });
---
+++ src/views/Livro/AdicionarLivroView.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3 class="pt-3">Adicionar Livro</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <form action="">
+                    <div class="form-group">
+                        <label for="">Nome: <strong>*</strong></label>
+                        <input type="text" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Descrição: </label>
+                        <textarea class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Autor: <strong>*</strong></label>
+                        <select class="form-select">
+                            <option selected>-----------</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Capa: </label>
+                        <input type="file" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="">Quantidade total: <strong>*</strong></label>
+                        <input type="text" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="">Quantidade em uso: <strong>*</strong></label>
+                        <input type="text" class="form-control" />
+                    </div>
+                    <button type="submit" class="btn btn-success">Cadastrar</button>
+                </form>
+            </div>
+            <div class="col-3"></div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
